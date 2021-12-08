73ff3137320f1699e1ca56f39406c7732e9bcc04
Charlie Parr <cparr4@apollo.snap.uaf.edu>
2021-12-07 15:24:11 -0900

    initial commit of helper script to download ak shapefile

---
+++ iem/gipl2_4km/download_ak_polygon.sh
@@ -0,0 +1,6 @@
+cd input_data/Permafrost/clipper_shp
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.prj
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.dbf
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.shp
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.shx
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.shp.xml
