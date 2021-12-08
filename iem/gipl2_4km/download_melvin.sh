97fbdb788504ee8a889ad60e13beeebc03b2f4f6
Charlie Parr <cparr4@apollo.snap.uaf.edu>
2021-12-07 15:23:50 -0900

    initial commit of helper script to grab source data

---
+++ iem/gipl2_4km/download_melvin.sh
@@ -0,0 +1,5 @@
+fileId=1NaZInm20tpnv63NcC5_p3xTWp5gC3Ty6
+fileName=Permafrost.zip
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
