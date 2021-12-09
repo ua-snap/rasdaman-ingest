034cad61f5f364b6a21d446fde32239fb0c638ae
Charlie Parr <cparr4@apollo.snap.uaf.edu>
2021-12-09 08:24:34 -0900

    update netcdf build - reordered dims

--- iem/gipl2_4km/crop_geotiff_dir_to_akpoly.sh
+++ iem/gipl2_4km/crop_geotiff_dir_to_akpoly.sh
@@ -1,2 +1,2 @@
 cd input_data/Permafrost/geotiff
-for i in `find . -type f -name "*.tif"`; do gdalwarp -crop_to_cutline -cutline ../clipper_shp/Alaska_Coast_Simplified_Polygon.shp -dstnodata -9999 -overwrite -t_srs EPSG:3338 -co COMPRESS=LZW $i "cropped"${i:1}; done
+for i in `find . -type f -name "*.tif"`; do gdalwarp -crop_to_cutline -cutline ../clipper_shp/Alaska_Coast_Simplified_Polygon.shp -dstnodata -9999.0 -overwrite -t_srs EPSG:3338 -co COMPRESS=LZW $i "cropped"${i:1}; done
