af02711765b7f11e33e9dfc0feda30d74724fc0d
Charles Parr <cparr4@alaska.edu>
2022-04-21 14:03:19 -0800

    update alfresco coverages and add styles with ingest hooks

---
+++ iem/alfresco/vegetation_type_pct/luts.py
@@ -0,0 +1,24 @@
+models = {
+    "MODEL-SPINUP": 0,
+    "GFDL-CM3": 1,
+    "GISS-E2-R": 2,
+    "IPSL-CM5A-LR": 3,
+    "MRI-CGCM3": 4,
+    "NCAR-CCSM4": 5,
+}
+
+scenarios = {
+    "historical": 0,
+    "rcp45": 1,
+    "rcp60": 2,
+    "rcp85": 3,
+  
+}
+
+eras = {
+    "1950-2008": 0,
+    "2010-2039": 1,
+    "2040-2069": 2,
+    "2070-2099": 3,
+}
+
