9d1e30c2e29e5549b57e639a42454d7f79f84948
Charlie Parr <cparr4@apollo.snap.uaf.edu>
2022-04-27 16:25:51 -0800

    add working ingest for huge geotiff stack

---
+++ arctic_eds/jan_min_mean_max_tas/luts.py
@@ -0,0 +1,21 @@
+tempstats = {
+    "tasmax": 0,
+    "tasmean": 1,
+    "tasmin": 2
+}
+
+models = {
+    "5modelAvg": 0,
+    "GFDL-CM3": 1,
+    "GISS-E2-R": 2,
+    "IPSL-CM5A-LR": 3,
+    "MRI-CGCM3": 4,
+    "NCAR-CCSM4": 5,
+}
+
+scenarios = {
+    "rcp45": 0,
+    "rcp60": 1,
+    "rcp85": 2,
+  
+}
