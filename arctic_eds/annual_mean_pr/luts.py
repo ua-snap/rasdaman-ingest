c22cafdbb825f4746a6dc3f9e36398723c335230
Kyle Redilla <kmredilla@alaska.edu>
2022-05-13 11:25:04 -0800

    Add notebook and ingest scripts for annual precipitation data (#30)

---
+++ arctic_eds/annual_mean_pr/luts.py
@@ -0,0 +1,16 @@
+models = {
+    "5modelAvg": 0,
+    "CRU-TS": 1,
+    "GFDL-CM3": 2,
+    "GISS-E2-R": 3,
+    "IPSL-CM5A-LR": 4,
+    "MRI-CGCM3": 5,
+    "NCAR-CCSM4": 6,
+}
+
+scenarios = {
+    "historical": 0,
+    "rcp45": 1,
+    "rcp60": 2,
+    "rcp85": 3,
+}
