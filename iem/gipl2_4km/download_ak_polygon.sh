e647f0f0e0ee7ff765c5e16c02202607b862fcb3
Charlie Parr <cparr4@alaska.edu>
2022-07-11 16:28:30 -0800

    Add styles from Apollo as ingest hooks (#42)
    * add arctic-eds styles for annual tas to ingest
    
    * add arctic-eds styles for annual_precip_totals_mm to ingest
    
    * add 4 arctic-eds styles for jan_min_mean_max to ingest
    
    * add 2 arctic-eds styles for freezing_index to ingest
    
    * add 2 arctic-eds styles for heating_degree_days to ingest
    
    * fix coverage id typo
    
    * add 2 arctic-eds styles for thawing_index to ingest
    
    * add 4 arctic-eds styles for july_min_mean_max to ingest
    
    * add NCR and EDS styles as pre-import hooks
    
    * json syntax typos
    
    * json syntax typo
    
    * json syntax typos
    
    * add missing = in WCPS query fragments of styles
    
    * download IEM with Aleutians shapefile instead of AK coast polygon for cropping
    
    * some tweaks for clarity plus re-execution to compare against existing coverage in Zeus
    
    * reformat ingest recipe (remove single quotes)
    
    * use intervals instead of ramp for new NCR style hook
    
    Co-authored-by: kyleredilla <kmredilla@alaska.edu>

--- iem/gipl2_4km/download_ak_polygon.sh
+++ iem/gipl2_4km/download_ak_polygon.sh
@@ -1,6 +1,6 @@
 cd input_data/Permafrost/clipper_shp
-wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.prj
-wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.dbf
-wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.shp
-wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.shx
-wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/alaska_coast_simplified/Alaska_Coast_Simplified_Polygon.shp.xml
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/iem_with_ak_aleutians/iem_with_ak_aleutians.prj
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/iem_with_ak_aleutians/iem_with_ak_aleutians.dbf
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/iem_with_ak_aleutians/iem_with_ak_aleutians.shp
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/iem_with_ak_aleutians/iem_with_ak_aleutians.shx
+wget --no-check-certificate https://raw.githubusercontent.com/ua-snap/geospatial-vector-veracity/main/vector_data/polygon/boundaries/iem_with_ak_aleutians/iem_with_ak_aleutians.cpg
