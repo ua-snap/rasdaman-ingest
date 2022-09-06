14d01443e8661efc5bdeda3539504f9d5c6c136e
kyleredilla <kmredilla@alaska.edu>
2022-09-06 12:08:02 -0800

    initial commit of template beetle risk ingest files

---
+++ beetles/luts.py
@@ -0,0 +1,23 @@
+models = {
+    "GFDL-ESM2M": 0,
+    "HadGEM2-ES":1,
+    "MRI-CGCM3": 2,
+    "NCAR-CCSM4": 3,
+}
+
+scenarios = {
+    "rcp45": 0,
+    "rcp85": 1,
+}
+
+eras = {
+    "2010-2039": 0,
+    "2040-2069": 1,
+    "2070-2099": 2,
+}
+
+snowpack = {
+    "low": 0,
+    "medium": 1,
+    "high": 2,
+}
