d25fd80116dd9ded4013534a5ac753d55cc58606
Kyle Redilla <kmredilla@alaska.edu>
2022-09-27 15:18:05 -0800

    Beetle risk (#50)
    * initial commit of template beetle risk ingest files
    
    * fix ordering of snowpack levels
    
    * remove high snowpack and add historical coordinates
    
    * updates for real risk class dataset
    
    * add README
    
    * update list of geotiffs
    
    * change daymet time period to 1988-2017

---
+++ beetles/README.md
@@ -0,0 +1,4 @@
+# Climate-driven spruce beetle outbreak risk (classes) projections for Alaska
+
+This folder contains the files for ingesting the beetle risk class dataset created using the [ua-snap/spruce-beetle-risk](https://github.com/ua-snap/spruce-beetle-risk) repository. 
+
---
+++ beetles/luts.py
@@ -0,0 +1,25 @@
+models = {
+    "daymet": 0,
+    "GFDL-ESM2M": 2,
+    "HadGEM2-ES":3,
+    "MRI-CGCM3": 4,
+    "CCSM4": 1,
+}
+
+scenarios = {
+    "hist": 0,
+    "rcp45": 1,
+    "rcp85": 2,
+}
+
+eras = {
+    "1988-2017": 0,
+    "2010-2039": 1,
+    "2040-2069": 2,
+    "2070-2099": 3,
+}
+
+snowpack = {
+    "low": 0,
+    "med": 1,
+}
