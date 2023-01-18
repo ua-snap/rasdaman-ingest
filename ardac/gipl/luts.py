13f47e0201eedec7ba207496f4e0817f6e1e7957
Charlie Parr <cparr4@alaska.edu>
2023-01-18 14:28:07 -0900

    Add GIPL ingest recipe (#57)
    Pre-processing and curation is in the ardac-curation repo

---
+++ ardac/gipl/luts.py
@@ -0,0 +1,23 @@
+models = {
+    "5ModelAvg": 0,
+    "GFDL-CM3": 1,
+    "NCAR-CCSM4": 2,
+}
+
+scenarios = {
+    "rcp45": 0,
+    "rcp85": 1,
+}
+
+pf_variables = {
+    "magt0.5m": 0,
+    "magt1m": 1,
+    "magt2m": 2,
+    "magt3m": 3,
+    "magt4m": 4,
+    "magt5m": 5,
+    "magtsurface": 6,
+    "permafrostbase": 7,
+    "permafrosttop": 8,
+    "talikthickness": 9,
+}
