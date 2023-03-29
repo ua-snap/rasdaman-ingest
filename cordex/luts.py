f3841b3348e301cf6dac67ca4a400f830e49795b
kyleredilla <kmredilla@alaska.edu>
2023-03-29 12:33:43 -0800

    initial commit of working notebook to process and ingest a coverage of summaries of indicators derived from the CORDEX dataset

---
+++ cordex/luts.py
@@ -0,0 +1,47 @@
+varnames = {
+    "cd": 0,
+    "cdd": 1,
+    "csdi": 2,
+    "cwd": 3,
+    "dw": 4,
+    "hd": 5,
+    "hsd": 6,
+    "r10mm": 7,
+    "rx1day": 8,
+    "rx5day": 9,
+    "su": 10,
+    "wndd": 11,
+    "wsdi": 12,
+}
+
+eras = {
+    "historical": 0,
+    "longterm": 1,
+    "midcentury": 2,
+}
+
+scenarios = {
+    "hist": 0,
+    "rcp45": 1,
+    "rcp85": 2,
+}
+
+stats = {
+    "max": 0,
+    "mean": 1,
+    "min": 2,
+}
+
+models = {
+    "CCCma-CanESM2_CCCma-CanRCM4": 0,
+    "CCCma-CanESM2_SMHI-RCA4": 1,
+    "CCCma-CanESM2_UQAM-CRCM5": 2,
+    "ICHEC-EC-EARTH_DMI-HIRHAM5": 3,
+    "ICHEC-EC-EARTH_SMHI-RCA4-SN": 4,
+    "ICHEC-EC-EARTH_SMHI-RCA4": 5,
+    "MPI-M-MPI-ESM-LR_MGO-RRCM": 6,
+    "MPI-M-MPI-ESM-LR_SMHI-RCA4-SN": 7,
+    "MPI-M-MPI-ESM-LR_SMHI-RCA4": 8,
+    "MPI-M-MPI-ESM-MR_UQAM-CRCM5": 9,
+    "NCC-NorESM1-M_SMHI-RCA4": 10,
+}
