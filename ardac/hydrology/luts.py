d8dc00ccdb91ee0a5489e7ff93e0aa238c06a4ca
Bob Torgerson <rltorgerson@alaska.edu>
2023-08-24 08:29:15 -0800

    Updates to fix issues.

--- ardac/hydrology/luts.py
+++ ardac/hydrology/luts.py
@@ -1,3 +1,18 @@
+variables = {
+    "evap": 0,
+    "glacier_melt": 1,
+    "iwe": 2,
+    "pcp": 3,
+    "runoff": 4,
+    "sm1": 5,
+    "sm2": 6,
+    "sm3": 7,
+    "snow_melt": 8,
+    "swe": 9,
+    "tmax": 10,
+    "tmin": 11,
+}
+
 models = {
     "ACCESS1-3": 0,
     "CCSM4": 1,
@@ -16,6 +31,21 @@ scenarios = {
     "rcp85": 1,
 }
 
+months = {
+    "apr": 0,
+    "aug": 1,
+    "dec": 2,
+    "feb": 3,
+    "jan": 4,
+    "jul": 5,
+    "jun": 6,
+    "mar": 7,
+    "may": 8,
+    "nov": 9,
+    "oct": 10,
+    "sep": 11,
+}
+
 eras = {
     "1950-1959": 0,
     "1960-1969": 1,
@@ -32,34 +62,4 @@ eras = {
     "2070-2079": 12,
     "2080-2089": 13,
     "2090-2099": 14,
-}
-
-months = {
-    "apr": 0,
-    "aug": 1,
-    "dec": 2,
-    "feb": 3,
-    "jan": 4,
-    "jul": 5,
-    "jun": 6,
-    "mar": 7,
-    "may": 8,
-    "nov": 9,
-    "oct": 10,
-    "sep": 11,
-}
-
-variables = {
-    "evap": 0,
-    "glacier_melt": 1,
-    "iwe": 2,
-    "pcp": 3,
-    "runoff": 4,
-    "sm1": 5,
-    "sm2": 6,
-    "sm3": 7,
-    "snow_melt": 8,
-    "swe": 9,
-    "tmax": 10,
-    "tmin": 11,
 }
