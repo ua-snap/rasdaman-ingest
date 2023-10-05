1062493eab9786e75d8bb6fd638e150aab2f2219
kyleredilla <kmredilla@alaska.edu>
2023-10-05 11:55:00 -0800

    initial commit of CMIP6 common grid daily tas ingest

---
+++ ardac/cmip6/tas_day_common/luts.py
@@ -0,0 +1,18 @@
+models = {
+    "CNRM-CM6-1-HR": 0,
+    "EC-Earth3-Veg": 1,
+    "GFDL-ESM4": 2,
+    "HadGEM3-GC31-LL": 3,
+    "HadGEM3-GC31-MM": 4,
+    "KACE-1-0-G": 5,
+    "MIROC6": 6,
+    "MPI-ESM1-2-LR": 7,
+}
+
+scenarios = {
+    "historical": 0,
+    "ssp126": 1,
+    "ssp245": 2,
+    "ssp370": 3,
+    "ssp585": 4,
+}
