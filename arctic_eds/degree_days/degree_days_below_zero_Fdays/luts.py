309284812180be9a972829aeb75c7dcb4ed323e3
Charlie Parr <cparr4@atlas17.snap.uaf.edu>
2024-04-01 16:33:47 -0800

    build ingest skeleton and set luts.py files

---
+++ arctic_eds/degree_days/degree_days_below_zero_Fdays/luts.py
@@ -0,0 +1,14 @@
+models = {
+    "ACCESS1-3": 0,
+    "CCSM4": 1,
+    "CSIRO-Mk3-6-0": 2,
+    "CanESM2": 3,
+    "daymet": 4,
+    "GFDL-ESM2M": 5,
+    "MIROC5": 6,
+    "MPI-ESM-MR": 7,
+    "MRI-CGCM3": 8,
+    "inmcm4": 9,
+}
+
+scenarios = {"historical": 0, "rcp45": 1, "rcp85": 2}
