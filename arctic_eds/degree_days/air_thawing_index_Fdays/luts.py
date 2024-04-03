09625f3c462e3476bdf8bfbd90c5a5e44f652a7d
Charlie Parr <cparr4@alaska.edu>
2024-04-03 15:07:33 -0800

    reorder models to account for case sorting

--- arctic_eds/degree_days/air_thawing_index_Fdays/luts.py
+++ arctic_eds/degree_days/air_thawing_index_Fdays/luts.py
@@ -1,14 +1,17 @@
+import numpy as np
+
 models = {
     "ACCESS1-3": 0,
     "CCSM4": 1,
     "CSIRO-Mk3-6-0": 2,
     "CanESM2": 3,
-    "daymet": 4,
-    "GFDL-ESM2M": 5,
-    "MIROC5": 6,
-    "MPI-ESM-MR": 7,
-    "MRI-CGCM3": 8,
+    "GFDL-ESM2M": 4,
+    "MIROC5": 5,
+    "MPI-ESM-MR": 6,
+    "MRI-CGCM3": 7,
+    "daymet": 8,
     "inmcm4": 9,
 }
 
 scenarios = {"historical": 0, "rcp45": 1, "rcp85": 2}
+
