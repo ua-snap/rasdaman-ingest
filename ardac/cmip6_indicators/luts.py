4e5ef4b0ea94149bc941e372ba8141cfeda844f0
Kyle Redilla <kmredilla@alaska.edu>
2024-08-06 16:31:02 -0800

    enterprise updates for cmip6_indicators coverage

--- ardac/cmip6_indicators/luts.py
+++ ardac/cmip6_indicators/luts.py
@@ -1,14 +1,16 @@
 models = {
-    "CNRM-CM6-1-HR": 0,
-    "EC-Earth3-Veg": 1,
-    "GFDL-ESM4": 2,
-    "HadGEM3-GC31-LL": 3,
-    "HadGEM3-GC31-MM": 4,
-    "KACE-1-0-G": 5,
-    "MIROC6": 6,
-    "MPI-ESM1-2-LR": 7,
-    "NorESM2-MM": 8,
-    "TaiESM1": 9,
+    "CESM2": 0,
+    "CESM2-WACCM": 1,
+    "CNRM-CM6-1-HR": 2,
+    "EC-Earth3-Veg": 3,
+    "GFDL-ESM4": 4,
+    "HadGEM3-GC31-LL": 5,
+    "HadGEM3-GC31-MM": 6,
+    "KACE-1-0-G": 7,
+    "MIROC6": 8,
+    "MPI-ESM1-2-LR": 9,
+    "NorESM2-MM": 10,
+    "TaiESM1": 11,
 }
 
 scenarios = {"historical": 0, "ssp126": 1, "ssp245": 2, "ssp370": 3, "ssp585": 4}
