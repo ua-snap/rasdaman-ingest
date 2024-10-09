fbf1287c13e8cc14ecc25f550861f91c3dfa0049
kyleredilla <kmredilla@alaska.edu>
2024-10-08 16:17:43 -0800

    update ingest and preprocessing script to add variable as axis to monthly cmip6 coverage

--- ardac/cmip6_common_grid/monthly/combine_regridded_data.py
+++ ardac/cmip6_common_grid/monthly/combine_regridded_data.py
@@ -1,5 +1,5 @@
 """Script to combine regridded CMIP6 data for ingestion into rasdaman. 
-This is just a simple wrapper for xarray's open_mfdataset function. 
+This is just a simple wrapper for xarray's open_mfdataset function to just group all yearly files into single years. 
 It is assumed that the data has already been regridded and is stored in the following 
 directory structure: <model>/<scenario>/<frequency (table ID)>/<variable ID>/<filename>. 
 The script will combine all files for all models, scenarios, and the supplied temporal frequency and the supplied variables into a single xarray dataset and write to disk.
@@ -34,73 +34,36 @@ def get_scenarios(regrid_dir, model):
     return [d.name for d in regrid_dir.joinpath(model).glob("*")]
 
 
-def get_files(var_ids, model, scenario, frequency, regrid_dir):
-    """Get a list of file paths for a given model, scenario, frequency, and variable IDs."""
-    fps = []
-    missing_variables = []
+def get_files(var_id, model, scenario, frequency, regrid_dir):
+    """Get a list of file paths for a given model, scenario, frequency, and variable ID."""
+    var_fps = list(regrid_dir.glob(f"{model}/{scenario}/*{frequency}/{var_id}/*.nc"))
 
-    for var_id in var_ids:
-        var_files = list(
-            regrid_dir.glob(f"{model}/{scenario}/*{frequency}/{var_id}/*.nc")
-        )
-        if len(var_files) > 0:
-            fps.extend(var_files)
-        else:
-            missing_variables.append(var_id)
-
-    return fps, missing_variables
+    return var_fps
 
 
-def add_missing_variable(ds, var_id):
-    """Add a missing variable to the dataset."""
-    nan_arr = np.empty((ds["time"].size, ds["lon"].size, ds["lat"].size))
-    nan_arr[:] = np.nan
-    empty_da = xr.DataArray(
-        nan_arr, coords=[ds["time"], ds["lon"], ds["lat"]], dims=["time", "lon", "lat"]
-    )
-    ds[var_id] = empty_da
-    ds[var_id].encoding = {
-        "dtype": np.float64,
-        "zlib": False,
-        "szip": False,
-        "zstd": False,
-        "bzip2": False,
-        "blosc": False,
-        "shuffle": False,
-        "complevel": 0,
-        "fletcher32": False,
-        "contiguous": True,
-        "chunksizes": None,
-        "original_shape": (ds["time"].shape[0], ds["lon"].shape[0], ds["lat"].shape[0]),
-        "_FillValue": np.nan,
-        "coordinates": "spatial_ref",
-    }
+def get_var_ids(var_group_id):
+    """Var_group_id is a string that contains the variable group ID, e.g. v1, v2, etc.
+    This is just a mapping that will use the config.py file."""
 
-    return ds
+    var_ids = var_group_id_lu[var_group_id]
 
-
-def add_missing_variables(ds, missing_variables):
-    """wrapper for add_missing_variables to add a list of missing variables to the dataset."""
-    for var_id in missing_variables:
-        ds = add_missing_variable(ds, var_id)
-
-    return ds
+    return var_ids
 
 
 def open_and_combine(
-    var_group_id, model, scenario, frequency, regrid_dir, rasda_dir, no_clobber
+    var_id, model, scenario, frequency, regrid_dir, rasda_dir, no_clobber
 ):
     """Rasda_dir is the directory where the combined dataset will be written to disk."""
-    var_ids = get_var_ids(var_group_id)
-
-    out_fp = rasda_dir.joinpath(f"{model}_{scenario}_{frequency}_{var_group_id}.nc")
+    out_fp = rasda_dir.joinpath(f"{var_id}_{model}_{scenario}_{frequency}.nc")
 
     if no_clobber and out_fp.exists():
         print(f"File {out_fp} already exists and no_clobber was supplied, skipping.")
         return
 
-    fps, missing_variables = get_files(var_ids, model, scenario, frequency, regrid_dir)
-    assert len(fps) > 0, f"No files found for {model}, {scenario}, {frequency}."
+    fps = get_files(var_id, model, scenario, frequency, regrid_dir)
+    if not len(fps) > 0:
+        print(f"No files found for {model}, {scenario}, {frequency}. Skipping.")
+        return
 
     ds = xr.open_mfdataset(
         fps,
@@ -109,15 +72,17 @@ def open_and_combine(
         preprocess=lambda x: x.drop_vars(["spatial_ref", "height"], errors="ignore"),
     )
 
-    ds = add_missing_variables(ds, missing_variables)
+    assert var_id in ds.data_vars
 
-    assert all(
-        var_id in ds.data_vars for var_id in var_ids
-    ), "Missing variables from combined dataset."
+    # all files should have same data variable name for ingestion into rasdaman
+    ds = ds.rename({var_id: "data"})
+
+    # ensure it is float type for consistency
+    ds["data"] = ds["data"].astype(np.float32)
 
     ds.to_netcdf(out_fp)
     print(
-        f"Combined data for {var_group_id} variables, {model}, {scenario}, {frequency} temporal resolution written to {out_fp}."
+        f"Combined data for {var_id}, {model}, {scenario}, {frequency} temporal resolution written to {out_fp}."
     )
 
 
@@ -129,15 +94,16 @@ def run_open_and_combine_for_all_groups(
 
     for model in models:
         for scenario in get_scenarios(regrid_dir, model):
-            open_and_combine(
-                var_group_id,
-                model,
-                scenario,
-                frequency,
-                regrid_dir,
-                rasda_dir,
-                no_clobber,
-            )
+            for var_id in get_var_ids(var_group_id):
+                open_and_combine(
+                    var_id,
+                    model,
+                    scenario,
+                    frequency,
+                    regrid_dir,
+                    rasda_dir,
+                    no_clobber,
+                )
 
 
 def parse_args():
@@ -186,3 +152,4 @@ if __name__ == "__main__":
     run_open_and_combine_for_all_groups(
         var_group_id, frequency, regrid_dir, rasda_dir, no_clobber
     )
+
--- ardac/cmip6_common_grid/monthly/luts.py
+++ ardac/cmip6_common_grid/monthly/luts.py
@@ -7,14 +7,47 @@ models = {
     "HadGEM3-GC31-MM": 5,
     "KACE-1-0-G": 6,
     "MIROC6": 7,
-    "MPI-ESM1-2-HR": 8,
-    "MRI-ESM2-0": 9,
-    "NorESM2-MM": 10,
-    "TaiESM1": 11, 
-}   
+    "MRI-ESM2-0": 8,
+    "NorESM2-MM": 9,
+    "TaiESM1": 10,
+    # MPI is the last model to be added
+    #  because it has no historical data
+    #  (currently as of 10/8/24, perhaps bug in processing)
+    "MPI-ESM1-2-HR": 11,
+}
+
+varname = {
+    "clt": 0,
+    "evspsbl": 1,
+    "hfls": 2,
+    "hfss": 3,
+    "pr": 4,
+    "psl": 5,
+    "rlds": 6,
+    "rsds": 7,
+    "sfcWind": 8,
+    "tas": 9,
+    "tasmax": 10,
+    "tasmin": 11,
+    "ts": 12,
+    "uas": 13,
+    "vas": 14,
+}
 
 var_group_id_lu = {
     "v1_1": ["tas", "tasmax", "tasmin", "pr", "sfcWind", "sfcWindmax"],
+    "v1_2": [
+        "clt",
+        "evspsbl",
+        "hfls",
+        "hfss",
+        "psl",
+        "rlds",
+        "rsds",
+        "ts",
+        "uas",
+        "vas",
+    ],
 }
 
 scenarios = {
@@ -24,3 +57,4 @@ scenarios = {
     "ssp370": 3,
     "ssp585": 4,
 }
+
