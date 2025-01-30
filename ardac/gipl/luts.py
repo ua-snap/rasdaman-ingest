1d9a67c2a72df6047c8e499faa648cf92929e4ee
Charlie Parr <cparr4@alaska.edu>
2025-01-30 10:58:22 -0900

    permafrost variables now bands, no longer needed in luts

--- ardac/gipl/luts.py
+++ ardac/gipl/luts.py
@@ -8,16 +8,3 @@ scenarios = {
     "rcp45": 0,
     "rcp85": 1,
 }
-
-pf_variables = {
-    "magt0.5m": 0,
-    "magt1m": 1,
-    "magt2m": 2,
-    "magt3m": 3,
-    "magt4m": 4,
-    "magt5m": 5,
-    "magtsurface": 6,
-    "permafrostbase": 7,
-    "permafrosttop": 8,
-    "talikthickness": 9,
-}
