e106b687a935cbac54c6994ecf69b82da0d3e2ce
Craig Stephenson <crstephenson@alaska.edu>
2025-04-09 14:10:26 -0800

    Initial commit of working climate_stripes coverage ingest.

---
+++ ardac/climate_stripes/luts.py
@@ -0,0 +1,24 @@
+models = {
+    "Berkeley-Earth": 0,
+    "CESM2": 1,
+    "CNRM-CM6-1-HR": 2,
+    "EC-Earth3-Veg": 3,
+    "GFDL-ESM4": 4,
+    "HadGEM3-GC31-LL": 5,
+    "HadGEM3-GC31-MM": 6,
+    "KACE-1-0-G": 7,
+    "MIROC6": 8,
+    "MPI-ESM1-2-HR": 9,
+    "MRI-ESM2-0": 10,
+    "NorESM2-MM": 11,
+    "TaiESM1": 12,
+}
+
+scenarios = {
+    "historical": 0,
+    "ssp126": 1,
+    "ssp245": 2,
+    "ssp370": 3,
+    "ssp585": 4,
+}
+
