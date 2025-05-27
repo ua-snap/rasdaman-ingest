27b0642bc94a1b195944a0077793b0753c0daaf3
Joshdpaul <jdpaul3@alaska.edu>
2025-05-27 07:59:42 -0800

    add all variable attributes from CMIP6 definitions

--- ardac/cmip6_common_grid/monthly/luts.py
+++ ardac/cmip6_common_grid/monthly/luts.py
@@ -3,7 +3,6 @@ import json
 cmip6_models = {
     "CESM2": 0,
     "CNRM-CM6-1-HR": 1,
-    # "E3SM-1-1": 2,
     "E3SM-2-0": 2,
     "EC-Earth3-Veg": 3,
     "GFDL-ESM4": 4,
@@ -26,25 +25,118 @@ cmip6_scenarios = {
     "ssp585": 4,
 }
 
+# attribute definitions for CMIP6 variables from https://airtable.com/appYNLuWqAgzLbhSq/shrKcLEdssxb8Yvcp/tblL7dJkC3vl5zQLb
 cmip6_var_attrs = {
-    "clt": {"units": "", "long_name": "", "description": ""},
-    "evspsbl": {"units": "", "long_name": "", "description": ""},
-    "hfls": {"units": "", "long_name": "", "description": ""},
-    "hfss": {"units": "", "long_name": "", "description": ""},
-    "pr": {"units": "", "long_name": "", "description": ""},
-    "prsn": {"units": "", "long_name": "", "description": ""},
-    "psl": {"units": "", "long_name": "", "description": ""},
-    "rlds": {"units": "", "long_name": "", "description": ""},
-    "rsds": {"units": "", "long_name": "", "description": ""},
-    "sfcWind": {"units": "", "long_name": "", "description": ""},
-    "siconc": {"units": "", "long_name": "", "description": ""},
-    "snw": {"units": "", "long_name": "", "description": ""},
-    "tas": {"units": "", "long_name": "", "description": ""},
-    "tasmax": {"units": "", "long_name": "", "description": ""},
-    "tasmin": {"units": "", "long_name": "", "description": ""},
-    "ts": {"units": "", "long_name": "", "description": ""},
-    "uas": {"units": "", "long_name": "", "description": ""},
-    "vas": {"units": "", "long_name": "", "description": ""},
+    "clt": {
+        "units": "percent",
+        "standard_name": "cloud_area_fraction",
+        "long_name": "Total Cloud Cover Percentage",
+        "description": "Total cloud area fraction (reported as a percentage) for the whole atmospheric column, as seen from the surface or the top of the atmosphere. Includes both large-scale and convective cloud.",
+    },
+    "evspsbl": {
+        "units": "kg m-2 s-1",
+        "standard_name": "water_evapotranspiration_flux",
+        "long_name": "Evapotranspiration Including Sublimation and Transpiration",
+        "description": "Evaporation at surface (also known as evapotranspiration): flux of water into the atmosphere due to conversion of both liquid and solid phases to vapor (from underlying surface and vegetation)",
+    },
+    "hfls": {
+        "units": "W m-2",
+        "standard_name": "surface_upward_latent_heat_flux",
+        "long_name": "Surface Upward Latent Heat Flux",
+        "description": "The surface called 'surface' means the lower boundary of the atmosphere. 'Upward' indicates a vector component which is positive when directed upward (negative downward). The surface latent heat flux is the exchange of heat between the surface and the air on account of evaporation (including sublimation). In accordance with common usage in geophysical disciplines, 'flux' implies per unit area, called 'flux density' in physics.",
+    },
+    "hfss": {
+        "units": "W m-2",
+        "standard_name": "surface_upward_sensible_heat_flux",
+        "long_name": "Surface Upward Sensible Heat Flux",
+        "description": "The surface sensible heat flux, also called turbulent heat flux, is the exchange of heat between the surface and the air by motion of air.",
+    },
+    "pr": {
+        "units": "mm",  # actual units are "kg m-2 s-1", but we convert these in regridding here: https://github.com/ua-snap/cmip6-utils/blob/d9ee45e1ed9e802896b0ee6e2c9d97eb5db7990d/regridding/regrid.py#L826
+        "standard_name": "precipitation_flux",
+        "long_name": "Precipitation",
+        "description": "Includes both liquid and solid phases.",
+    },
+    "prsn": {
+        "units": "mm",  # actual units are "kg m-2 s-1", but we convert these in regridding here: https://github.com/ua-snap/cmip6-utils/blob/d9ee45e1ed9e802896b0ee6e2c9d97eb5db7990d/regridding/regrid.py#L826
+        "standard_name": "snowfall_flux",
+        "long_name": "Snowfall Flux",
+        "description": "At surface; includes precipitation of all forms of water in the solid phase.",
+    },
+    "psl": {
+        "units": "Pa",
+        "standard_name": "air_pressure_at_mean_sea_level",
+        "long_name": "Sea Level Pressure",
+        "description": "Sea Level Pressure.",
+    },
+    "rlds": {
+        "units": "W m-2",
+        "standard_name": "surface_downwelling_longwave_flux_in_air",
+        "long_name": "Surface Downwelling Longwave Radiation",
+        "description": "The surface called 'surface' means the lower boundary of the atmosphere. 'longwave' means longwave radiation. Downwelling radiation is radiation from above. It does not mean 'net downward'. When thought of as being incident on a surface, a radiative flux is sometimes called 'irradiance'. In addition, it is identical with the quantity measured by a cosine-collector light-meter and sometimes called 'vector irradiance'. In accordance with common usage in geophysical disciplines, 'flux' implies per unit area, called 'flux density' in physics.",
+    },
+    "rsds": {
+        "units": "W m-2",
+        "standard_name": "surface_downwelling_shortwave_flux_in_air",
+        "long_name": "Surface Downwelling Shortwave Radiation",
+        "description": "Surface solar irradiance for UV calculations.",
+    },
+    "sfcWind": {
+        "units": "m s-1",
+        "standard_name": "wind_speed",
+        "long_name": "Near-Surface Wind Speed",
+        "description": "Near-surface (usually, 10 meters) wind speed.",
+    },
+    "siconc": {
+        "units": "",
+        "standard_name": "",
+        "long_name": "",
+        "description": "",
+    },
+    "snw": {
+        "units": "percent",
+        "standard_name": "sea_ice_area_fraction",
+        "long_name": "Sea Ice Area Percentage",
+        "description": "Percentage of grid cell covered by sea ice.",
+    },
+    "tas": {
+        "units": "degC",  # actual units are "K", but we convert these in regridding here: https://github.com/ua-snap/cmip6-utils/blob/d9ee45e1ed9e802896b0ee6e2c9d97eb5db7990d/regridding/regrid.py#L826
+        "standard_name": "air_temperature",
+        "long_name": "Near-Surface Air Temperature",
+        "description": "Near-surface (usually, 2 meter) air temperature.",
+    },
+    "tasmax": {
+        "units": "degC",  # actual units are "K", but we convert these in regridding here: https://github.com/ua-snap/cmip6-utils/blob/d9ee45e1ed9e802896b0ee6e2c9d97eb5db7990d/regridding/regrid.py#L826
+        "standard_name": "air_temperature",
+        "long_name": "Daily Maximum Near-Surface Air Temperature",
+        "description": "Maximum near-surface (usually, 2 meter) air temperature.",
+        "cell_methods": "time: max",  # added for CF convention since standard_name is ambiguous
+    },
+    "tasmin": {
+        "units": "degC",  # actual units are "K", but we convert these in regridding here: https://github.com/ua-snap/cmip6-utils/blob/d9ee45e1ed9e802896b0ee6e2c9d97eb5db7990d/regridding/regrid.py#L826
+        "standard_name": "air_temperature",
+        "long_name": "Daily Minimum Near-Surface Air Temperature",
+        "description": "Minimum near-surface (usually, 2 meter) air temperature.",
+        "cell_methods": "time: min",  # added for CF convention since standard_name is ambiguous
+    },
+    "ts": {
+        "units": "K",
+        "standard_name": "surface_temperature",
+        "long_name": "Surface Temperature",
+        "description": "Temperature of the lower boundary of the atmosphere.",
+    },
+    "uas": {
+        "units": "m s-1",
+        "standard_name": "eastward_wind",
+        "long_name": "Eastward Near-Surface Wind",
+        "description": "Eastward component of the near-surface (usually, 10 meters) wind.",
+    },
+    "vas": {
+        "units": "m s-1",
+        "standard_name": "northward_wind",
+        "long_name": "Northward Near-Surface Wind",
+        "description": "Northward component of the near-surface (usually, 10 meters) wind.",
+    },
 }
 
 
@@ -55,7 +147,7 @@ rasdaman_encodings = {
 }
 
 global_attrs = {
-    # "Conventions": "CF-1.8",
+    "Conventions": "CF-1.8",
     "title": "CMIP6 regridded data",
     "institution": "Scenarios Network for Alaska and Arctic Planning, University of Alaska Fairbanks, International Arctic Research Center",
     "source": "CMIP6 model output",
