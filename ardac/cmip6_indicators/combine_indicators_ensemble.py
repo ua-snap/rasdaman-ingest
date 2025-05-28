113baef2316e065357d9dfe1faefe6f96846b98b
Joshdpaul <jdpaul3@alaska.edu>
2025-05-28 15:11:04 -0800

    start adding functions / update luts

---
+++ ardac/cmip6_indicators/combine_indicators_ensemble.py
@@ -0,0 +1,119 @@
+"""Script to combine CMIP6 indicators data for ingestion into rasdaman.
+This will combine all CMIP6 indicator files into one netCDF, storing variables as data_vars and computing an ensemble mean for all variables.
+It is assumed that the indicators have already been computed and stored in the following
+directory structure: <model>/<scenario>/<indicator ID>/<filename>.
+The script will combine all files for supplied models, scenarios, and indicators into a single xarray dataset and write to disk.
+
+example usage:
+  python combine_indicators_ensemble.py --models 'all' --scenarios 'all' --indicators 'all' --indicators_dir /beegfs/CMIP6/jdpaul3/cmip6_indicators_test/cmip6_indicators/netcdf --rasda_dir /beegfs/CMIP6/jdpaul3/cmip6_indicators_for_rasdaman
+"""
+
+import argparse
+import sys
+import subprocess
+import xarray as xr
+import rioxarray
+from pathlib import Path
+from datetime import datetime
+from luts import (
+    cmip6_models,
+    cmip6_scenarios,
+    cmip6_indicator_attrs,
+    description_fmt_str,
+    title_fmt_str,
+    global_attrs,
+)
+
+
+def validate_all_args(models, scenarios, indicators, indicators_dir, rasda_dir):
+    """Validate all input arguments for the script."""
+
+    # validate dirs
+    if not rasda_dir.exists():
+        rasda_dir.mkdir()
+    if not indicators_dir.exists():
+        sys.exit(f"Directory not found: {indicators_dir}")
+
+    # validate models/scenarios/vars
+    if models == "all":
+        models = list(cmip6_models.keys())
+        models.remove("Ensemble")  # drop "Ensemble" from models list
+    else:
+        models = models.split()
+        validate_args_against_dict(models, cmip6_models)
+
+    if scenarios == "all":
+        scenarios = list(cmip6_scenarios.keys())
+    else:
+        scenarios = scenarios.split()
+        validate_args_against_dict(scenarios, cmip6_scenarios)
+
+    if vars == "all":
+        vars = list(cmip6_indicator_attrs.keys())
+    else:
+        vars = vars.split()
+        validate_args_against_dict(vars, cmip6_indicator_attrs)
+
+    return models, scenarios, indicators, indicators_dir, rasda_dir
+
+
+def validate_args_against_dict(input_list, cmip6_dict):
+    """Validate a list of arguments against a list derived from dictionary keys."""
+
+    for item in input_list:
+        if item not in list(cmip6_dict.keys()):
+            sys.exit(
+                f"Input {item} not allowed. Must be one of {list(cmip6_dict.keys())}"
+            )
+        else:
+            pass
+
+
+# TODO flesh out the functions
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(
+        description="Combine CMIP6 indicators for ingestion into rasdaman."
+    )
+    parser.add_argument(
+        "--models",
+        type=str,
+        help="[ ]-separated string of model names (e.g. 'CESM2 GFDL-ESM4'), or 'all' for all models.",
+    )
+    parser.add_argument(
+        "--scenarios",
+        type=str,
+        help="[ ]-separated list of scenario names (e.g. 'historical ssp585'), or 'all' for all scenarios.",
+    )
+    parser.add_argument(
+        "--indicators",
+        type=str,
+        help="[ ]-separated list of indicator names (e.g. 'dw su'), or 'all' for all indicators.",
+    )
+    parser.add_argument(
+        "--indicators_dir", type=str, help="Directory where indicators data is stored."
+    )
+    parser.add_argument(
+        "--rasda_dir",
+        type=str,
+        help="Directory where combined data will be written to disk.",
+    )
+
+    args = parser.parse_args()
+
+    return {
+        "models": args.models,
+        "scenarios": args.scenarios,
+        "indicators": args.indicators,
+        "indicators_dir": Path(args.indicators_dir),
+        "rasda_dir": Path(args.rasda_dir),
+    }
+
+
+if __name__ == "__main__":
+
+    models, scenarios, indicators, indicators_dir, rasda_dir = validate_all_args(
+        **parse_args()
+    )
--- ardac/cmip6_indicators/luts.py
+++ ardac/cmip6_indicators/luts.py
@@ -1,16 +1,50 @@
-models = {
+cmip6_models = {
     "CESM2": 0,
     "CNRM-CM6-1-HR": 1,
-    "EC-Earth3-Veg": 2,
-    "GFDL-ESM4": 3,
-    "HadGEM3-GC31-LL": 4,
-    "HadGEM3-GC31-MM": 5,
-    "KACE-1-0-G": 6,
-    "MIROC6": 7,
-    "MPI-ESM1-2-HR": 8,
-    "MRI-ESM2-0": 9,
-    "NorESM2-MM": 10,
-    "TaiESM1": 11,
+    "E3SM-2-0": 2,
+    "EC-Earth3-Veg": 3,
+    "GFDL-ESM4": 4,
+    "HadGEM3-GC31-LL": 5,
+    "HadGEM3-GC31-MM": 6,
+    "KACE-1-0-G": 7,
+    "MIROC6": 8,
+    "MPI-ESM1-2-HR": 9,
+    "MRI-ESM2-0": 10,
+    "NorESM2-MM": 11,
+    "TaiESM1": 12,
+    "Ensemble": 13,
 }
 
-scenarios = {"historical": 0, "ssp126": 1, "ssp245": 2, "ssp370": 3, "ssp585": 4}
+cmip6_scenarios = {
+    "historical": 0,
+    "ssp126": 1,
+    "ssp245": 2,
+    "ssp370": 3,
+    "ssp585": 4,
+}
+
+cmip6_indicator_attrs = {
+    "dw": {
+        "units": "",
+        "standard_name": "",
+        "long_name": "",
+        "description": "",
+    },
+    # TODO: add all the indicator attributes here
+}
+
+
+title_fmt_str = (
+    "CMIP6 Climate Indicator Data on a Common Grid with Multi-Model Ensemble Mean"
+)
+description_fmt_str = "Climate Indicator data from {number_of_models} CMIP6 models on a common grid, including multi-model ensemble mean calculated for each indicator. Models include {models}. Scenarios include {scenarios}. Indicators include {variables}. Multi-model ensemble mean is calculated for each indicator across all available models for each scenario; some indicators do not have data for all models or scenarios."
+
+global_attrs = {
+    "Conventions": "CF-1.8",
+    "title": "",
+    "description": "",
+    "institution": "Scenarios Network for Alaska and Arctic Planning, University of Alaska Fairbanks, International Arctic Research Center",
+    "source": "CMIP6 model output",
+    "contact": "uaf-snap-data-tools@alaska.edu",
+    "url": "https://uaf-snap.org/",
+}
