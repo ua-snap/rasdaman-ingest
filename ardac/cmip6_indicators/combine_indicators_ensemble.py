c5a2e1571073747c16c619a0db61a153e3a86804
Joshdpaul <jdpaul3@alaska.edu>
2025-06-24 13:27:52 -0800

    add missing pandas import

--- ardac/cmip6_indicators/combine_indicators_ensemble.py
+++ ardac/cmip6_indicators/combine_indicators_ensemble.py
@@ -14,6 +14,7 @@ import subprocess
 import xarray as xr
 import rioxarray
 import numpy as np
+import pandas as pd
 from pathlib import Path
 from datetime import datetime
 from dask.distributed import Client
