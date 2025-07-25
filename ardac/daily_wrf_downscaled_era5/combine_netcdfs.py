1934f733fd065d48fd12c7e251b1952debec4ef5
SNAP Data User <cparr4@alaska.edu>
2025-07-25 11:15:46 -0800

    less threads, more sleep

--- ardac/daily_wrf_downscaled_era5/combine_netcdfs.py
+++ ardac/daily_wrf_downscaled_era5/combine_netcdfs.py
@@ -1,7 +1,9 @@
-import xarray as xr
 import glob
+import time
 import os
 import sys
+
+import xarray as xr
 from dask.distributed import Client, LocalCluster
 
 
@@ -12,7 +14,7 @@ def merge_netcdfs_final(variable_name):
     input_dir = variable_name
     output_file = f"{variable_name}_merged.nc"
 
-    print(f"Starting final preprocessing script for {variable_name}...")
+    print(f"Starting netCDF merge script for {variable_name}...")
 
     try:
         search_pattern = os.path.join(input_dir, "*.nc")
@@ -38,10 +40,10 @@ def merge_netcdfs_final(variable_name):
 
             # Fire up the Dask cluster to handle the I/O-heavy write operation.
             cluster = LocalCluster(
-                n_workers=36, threads_per_worker=36, memory_limit="8GB"
+                n_workers=44, threads_per_worker=2, memory_limit="8GB"
             )
             client = Client(cluster)
-            print(f"--> Dask dashboard available at: {client.dashboard_link} <--")
+            print(f"Dask dashboard available at: {client.dashboard_link}")
 
             try:
                 print("Preparing parallel write operation...")
@@ -61,16 +63,17 @@ def merge_netcdfs_final(variable_name):
                 print("Executing parallel write.")
                 write_job.compute()
             finally:
+                time.sleep(5)
                 # Always ensure the cluster is shut down
                 print("Shutting down Dask client and cluster.")
                 client.close()
                 cluster.close()
+                time.sleep(10)
 
         print("Preprocessing complete. Merged file created successfully.")
 
     except Exception as e:
         print(f"An error occurred during processing: {e}", file=sys.stderr)
-        sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -78,3 +81,4 @@ if __name__ == "__main__":
         print("Usage: python combine_netcdfs.py <variable_name>")
         sys.exit(1)
     merge_netcdfs_final(sys.argv[1])
+    time.sleep(10)
