aea8e491cce38e11ac3b1a3a2f31998c23c3e4dd
Craig Stephenson <crstephenson@alaska.edu>
2025-09-15 09:35:30 -0800

    Add multiple models to test coverage.

--- ardac/cmip6_downscaled/luts.py
+++ ardac/cmip6_downscaled/luts.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+models = {
+    "CNRM-CM6-1-HR": 0,
+    "EC-Earth3-Veg": 1,
+    "GFDL-ESM4": 2,
+    "HadGEM3-GC31-LL": 3,
+    "HadGEM3-GC31-MM": 4,
+    "KACE-1-0-G": 5,
+    "MIROC6": 6,
+    "MPI-ESM1-2-HR": 7,
+    "MRI-ESM2-0": 8,
+    "NorESM2-MM": 9,
+    "TaiESM1": 10
+}
+
 scenarios = {
     "historical": 0,
     "ssp126": 1,
