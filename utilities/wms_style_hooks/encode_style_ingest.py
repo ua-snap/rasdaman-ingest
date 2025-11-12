ea779e244b3f7648b0e02f7af7ed1b70ba308cdb
Craig Stephenson <crstephenson@alaska.edu>
2025-11-11 16:30:00 -0900

    Add encode_style_ingest.py script, example JSON file, and README.

---
+++ utilities/wms_style_hooks/encode_style_ingest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import json
+import sys
+
+with open(sys.argv[1]) as f:
+    items = json.load(f)
+
+for item in items:
+    encoded = {}
+    for key, value in item.items():
+        if key == "wcps":
+            encoded[key] = (
+                value.replace(" ", "%20")
+                .replace("$", "%24")
+                .replace("(", "%28")
+                .replace(")", "%29")
+                .replace("+", "%2B")
+                .replace(",", "%2C")
+                .replace("/", "%2F")
+                .replace(":", "%3A")
+                .replace("[", "%5B")
+                .replace("]", "%5D")
+                .replace('"', "%22")
+            )
+        elif key == "abstract":
+            encoded[key] = value.replace(" ", "%20")
+        elif key == "colormap":
+            encoded[key] = value.replace(" ", "").replace('"', '\\\\\\"')
+        else:
+            encoded[key] = value
+
+    cmd_string = """{{
+      \"description\": \"{cmd_description}\",
+      \"when\": \"after_import\",
+      \"cmd\": \". /etc/default/rasdaman; curl --user $RASCURL \\"https://localhost/rasdaman/admin/layer/style/add?COVERAGEID={coverage}&STYLEID={name}&ABSTRACT={abstract}&WCPSQUERYFRAGMENT={wcps}&COLORTABLETYPE=ColorMap&\\" --data-urlencode \\"COLORTABLEDEFINITION={colormap}\\"",
+      \"abort_on_error\": true
+}},"""
+    print(cmd_string.format(**encoded))
