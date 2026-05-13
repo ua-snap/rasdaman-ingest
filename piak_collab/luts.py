dbf116b05adc295056a3c37c7aa4a8a41b336a40
Craig Stephenson <crstephenson@alaska.edu>
2026-05-12 16:46:28 -0800

    Successful ingest of means coverage.

--- piak_collab/luts.py
+++ piak_collab/luts.py
@@ -1,20 +1,20 @@
-model = {
+models = {
     "ACCESS-CM2": 0,
 }
 
-scenario = {
+scenarios = {
     "ssp126": 0,
     "ssp245": 1,
     "ssp370": 2,
     "ssp585": 3,
 }
 
-position = {
+positions = {
     "MID": 0,
     "END": 1,
 }
 
-season = {
+seasons = {
     "ANNUAL": 0,
     "DRY": 1,
     "WET": 2
