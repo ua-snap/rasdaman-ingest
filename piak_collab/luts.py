980a62437d965e62ff12e85838d1d4e6aec58282
Craig Stephenson <crstephenson@alaska.edu>
2026-05-12 16:01:49 -0800

    Fix min/max issue in ingest script.

---
+++ piak_collab/luts.py
@@ -0,0 +1,21 @@
+model = {
+    "ACCESS-CM2": 0,
+}
+
+scenario = {
+    "ssp126": 0,
+    "ssp245": 1,
+    "ssp370": 2,
+    "ssp585": 3,
+}
+
+position = {
+    "MID": 0,
+    "END": 1,
+}
+
+season = {
+    "ANNUAL": 0,
+    "DRY": 1,
+    "WET": 2
+}
