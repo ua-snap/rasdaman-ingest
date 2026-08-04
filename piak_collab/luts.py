ef053b642b448273c09f6ad683bf52c32823b057
Craig Stephenson <crstephenson@alaska.edu>
2026-08-03 21:27:08 -0800

    Revamp PI-AK NetCDF combine script to include all 30 models and pct/abs deltas.

--- piak_collab/luts.py
+++ piak_collab/luts.py
@@ -1,17 +1,48 @@
 models = {
     "ACCESS-CM2": 0,
+    "ACCESS-ESM1-5": 1,
+    "BCC-CSM2-MR": 2,
+    "CanESM5": 3,
+    "CMCC-ESM2": 4,
+    "CNRM-CM6-1": 5,
+    "CNRM-ESM2-1": 6,
+    "EC-Earth3": 7,
+    "EC-Earth3-Veg-LR": 8,
+    "FGOALS-g3": 9,
+    "GFDL-CM4": 10,
+    "GFDL-ESM4": 11,
+    "GISS-E2-1-G": 12,
+    "HadGEM3-GC31-LL": 13,
+    "HadGEM3-GC31-MM": 14,
+    "INM-CM4-8": 15,
+    "INM-CM5-0": 16,
+    "IPSL-CM6A-LR": 17,
+    "KACE-1-0-G": 18,
+    "KIOST-ESM": 19,
+    "MIROC6": 20,
+    "MIROC-ES2L": 21,
+    "MPI-ESM1-2-HR": 22,
+    "MPI-ESM1-2-LR": 23,
+    "MRI-ESM2-0": 24,
+    "NESM3": 25,
+    "NorESM2-LM": 26,
+    "NorESM2-MM": 27,
+    "TaiESM1": 28,
+    "UKESM1-0-LL": 29
 }
 
 scenarios = {
-    "ssp126": 0,
-    "ssp245": 1,
-    "ssp370": 2,
-    "ssp585": 3,
+    "HIST": 0,
+    "ssp126": 1,
+    "ssp245": 2,
+    "ssp370": 3,
+    "ssp585": 4,
 }
 
 positions = {
-    "MID": 0,
-    "END": 1,
+    "HIST": 0,
+    "MID": 1,
+    "END": 2,
 }
 
 seasons = {
